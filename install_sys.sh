a2a4986c4682176975f535e6bb75f7d7b0f9a9e0
matthieu <matthieu.cneude@gmail.com>
2021-01-29 10:25:08 +0100

    Fix little mistake

--- install_sys.sh
+++ install_sys.sh
@@ -15,8 +15,6 @@ Are you sure?" 15 60 || exit
 dialog --no-cancel --inputbox "Enter a name for your computer." \
     10 60 2> comp
 
-comp=$(cat comp) && rm comp
-
 # Verify boot (UEFI or BIOS)
 uefi=0
 ls /sys/firmware/efi/efivars 2> /dev/null && uefi=1
@@ -29,7 +27,7 @@ dialog --title "Choose your hard drive" --no-cancel --radiolist \
 "Where do you want to install your new system? \n\n\
 Select with SPACE, valid with ENTER. \n\n\
 WARNING: Everything will be DESTROYED on the hard disk!" \
-15 60 4 "${devices_list[@]}" 2> hd 
+15 60 4 "${devices_list[@]}" 2> hd
 
 hd=$(cat hd) && rm hd
 
